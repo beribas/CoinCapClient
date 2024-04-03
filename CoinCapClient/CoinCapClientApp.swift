5438f10e4d68fd6715c29b3bff16be96d87c86fd
Oleg Langer <piligrimo@gmail.com>
2024-04-03 15:53:20 +0200

    Initial Commit

---
+++ CoinCapClient/CoinCapClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCapClientApp.swift
+//  CoinCapClient
+//
+//  Created by Oleg Langer on 03.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CoinCapClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
